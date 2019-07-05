7c9961c93245b69a0e03626e1353ef658a3be506
childish-sambino <sharrison@twilio.com>
2019-07-05 14:17:55 -0500

    breaking: Make the API browser more generic (#24)
    Drops the 'version' grouping and adds HTTP method/operation names.

--- src/services/twilio-api/api-browser.js
+++ src/services/twilio-api/api-browser.js
@@ -1,90 +1,62 @@
 const fs = require('fs');
-const url = require('url');
-const { doesObjectHaveProperty } = require('../javascript-utilities');
-const ResourcePathParser = require('../resource-path-parser');
+const { camelCase } = require('../naming-conventions');
+let apiSpec; // Lazy-loaded below.
 
-const isApi2010 = (domain, version) => {
-  return domain === 'api' && (version === '2010-04-01' || version === 'v2010');
-};
-
-function translateLegacyVersions(domain, version) {
-  // In the Node helper library, api.twilio.com/2010-04-01 is represented as "v2010"
-  return isApi2010(domain, version) ? 'v2010' : version;
-}
-
-const listResourceMethodMap = {
-  get: 'list',
-  post: 'create'
-};
-
-const instanceResourceMethodMap = {
-  delete: 'remove',
-  get: 'fetch',
-  post: 'update'
-};
-
-/*
-
-Notes:
-
-Disambiguating like-named resources:
-If same resource name exists under multiple domains/versions/paths, then have the command
-map to a help message when it is ambiguous as to which domain/version/path they want.
-Maybe have a setting to change this behavior? Have a custom shortcut map in the config
-file? Have a mode of operation where it just picks the most recent version?
-
-*/
+const OPERATIONS = ['post', 'get', 'delete'];
 
 class TwilioApiBrowser {
   constructor(apiSpec) {
-    this.apiSpec = apiSpec || this.loadApiSpecFromDisk();
-    this.domains = this.loadDomains();
+    apiSpec = apiSpec || this.loadApiSpecFromDisk();
+    this.domains = this.loadDomains(apiSpec);
   }
 
   loadApiSpecFromDisk() {
-    // Assume all 'json' files in here are OpenAPI spec files.
-    return fs.readdirSync(__dirname)
-      .filter(filename => filename.endsWith('.json'))
-      .map(filename => require(`./${filename}`));
+    if (!apiSpec) {
+      const specPattern = /twilio_(.+)\.json/;
+      const specNameIndex = 1;
+
+      apiSpec = fs.readdirSync(__dirname)
+        .filter(filename => filename.match(specPattern))
+        .map(filename => {
+          const domainName = filename.match(specPattern)[specNameIndex];
+
+          return { [domainName]: require(`./${filename}`) };
+        });
+
+      apiSpec = Object.assign({}, ...apiSpec);
+    }
+
+    return apiSpec;
   }
 
-  loadDomains() {
-    const domains = {};
+  loadDomains(apiSpec) {
+    const domains = apiSpec;
 
-    this.apiSpec.forEach(spec => {
-      Object.keys(spec.paths).forEach(path => {
-        // Naive assumption: The Twilio API's only have a single domain
-        const serverUrl = new url.URL(spec.paths[path].servers[0].url);
-        const domain = serverUrl.host.split('.')[0]; // e.g. 'api' from 'api.twilio.com'
+    Object.values(domains).forEach(spec => {
+      Object.values(spec.paths).forEach(path => {
+        // Naive assumption: The Twilio APIs only have a single server.
+        path.server = path.servers[0].url;
+        delete path.servers;
 
-        const resourcePathParser = new ResourcePathParser(path);
-        resourcePathParser.normalizePath(); // e.g /v1/foo/bar/{Sid}.json --> /foo/bar
-        const resourcePath = resourcePathParser.getFullPath();
+        path.operations = {};
+        path.description = path.description.replace(/(\r\n|\n|\r)/gm, ' ');
 
-        const version = translateLegacyVersions(domain, resourcePathParser.version);
+        // Move the operations into an operations object.
+        OPERATIONS.forEach(operationName => {
+          if (operationName in path) {
+            path.operations[operationName] = path[operationName];
+            delete path[operationName];
+          }
+        });
 
-        if (!doesObjectHaveProperty(domains, domain)) {
-          domains[domain] = { versions: {}, tags: spec.tags };
-        }
+        // Convert extensions to camel-cased properties.
+        Object.entries(path).forEach(([key, value]) => {
+          const extensionMatch = key.match(/x-(.+)/);
 
-        if (!doesObjectHaveProperty(domains[domain].versions, version)) {
-          domains[domain].versions[version] = { resources: {} };
-        }
-
-        const resources = domains[domain].versions[version].resources;
-        if (!doesObjectHaveProperty(resources, resourcePath)) {
-          resources[resourcePath] = {
-            actions: {},
-            description: spec.paths[path].description.replace(/(\r\n|\n|\r)/gm, ' '),
-            defaultOutputProperties: spec.paths[path]['x-default-output-properties']
-          };
-        }
-
-        const actions = resources[resourcePath].actions;
-        const methodMap = resourcePathParser.isInstanceResource ? instanceResourceMethodMap : listResourceMethodMap;
-        Object.keys(methodMap).forEach(method => {
-          if (doesObjectHaveProperty(spec.paths[path], method)) {
-            actions[methodMap[method]] = spec.paths[path][method];
+          if (extensionMatch) {
+            const newKey = camelCase(extensionMatch[1]);
+            path[newKey] = value;
+            delete path[key];
           }
         });
       });
@@ -94,7 +66,4 @@ class TwilioApiBrowser {
   }
 }
 
-module.exports = {
-  TwilioApiBrowser,
-  isApi2010
-};
+module.exports = TwilioApiBrowser;
