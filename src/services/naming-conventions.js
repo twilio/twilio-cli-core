c4dc3500dd6a26284aa012ed8b38e3cacd0d2de1
Sam Harrison <sharrison@twilio.com>
2019-07-30 14:58:57 -0500

    Handle dot-separated strings when converting to camelCase (or PascalCase)

--- src/services/naming-conventions.js
+++ src/services/naming-conventions.js
@@ -11,8 +11,9 @@ const kebabCase = input => {
 const camelCase = input => {
   return input
     .trim()
-    .replace(/^[-_]+|[-_]+$/g, '')// remove leading and trailing dashed and underscores
+    .replace(/^[-_]+|[-_]+$/g, '')// remove leading and trailing dashes and underscores
     .replace(/^[A-Z]/, g => g[0].toLowerCase()) // from PascalCase
+    .replace(/\.[A-Z]/g, g => g.toLowerCase()) // from dot-separated
     .replace(/[A-Z]{2,}/g, g => g.toLowerCase()) // consecutive caps (e.g. "AWS")  TODO: What about AWSRoute53?
     .replace(/[-_ ]([a-z])/g, g => g[1].toUpperCase()) // from kebab-case or snake_case (or spaces)
     .replace(/ /g, ''); // remove any remaining spaces
@@ -32,7 +33,10 @@ const capitalize = input => {
   return input.trim().replace(/^[a-z]/, g => g[0].toUpperCase()); // upper the first character
 };
 
-const pascalCase = input => capitalize(camelCase(input));
+const pascalCase = input => {
+  return camelCase(input) // camelize first
+    .replace(/(^|\.)[a-z]/g, g => g.toUpperCase()); // upper the first character and after each dot
+};
 
 module.exports = {
   kebabCase,
--- test/services/naming-conventions.test.js
+++ test/services/naming-conventions.test.js
@@ -1,4 +1,4 @@
-const { kebabCase, camelCase, snakeCase, capitalize } = require('../../src/services/naming-conventions');
+const { kebabCase, camelCase, pascalCase, snakeCase, capitalize } = require('../../src/services/naming-conventions');
 
 const { expect, test } = require('@twilio/cli-test');
 
@@ -7,25 +7,36 @@ describe('services', () => {
     test.it('handles single word', () => {
       expect(kebabCase('one')).to.equal('one');
       expect(camelCase('one')).to.equal('one');
+      expect(pascalCase('one')).to.equal('One');
       expect(snakeCase('one')).to.equal('one');
     });
 
     test.it('handles all caps word', () => {
       expect(kebabCase('ONE')).to.equal('one');
       expect(camelCase('ONE')).to.equal('one');
+      expect(pascalCase('ONE')).to.equal('One');
       expect(snakeCase('ONE')).to.equal('one');
     });
 
     test.it('trims leading and trailing spaces', () => {
       expect(kebabCase('  one  ')).to.equal('one');
       expect(camelCase('  one  ')).to.equal('one');
+      expect(pascalCase('  one  ')).to.equal('One');
       expect(snakeCase('  one  ')).to.equal('one');
     });
 
     test.it('trims leading and trailing symbols', () => {
       expect(kebabCase('___one___')).to.equal('one');
       expect(camelCase('___one___')).to.equal('one');
+      expect(pascalCase('___one___')).to.equal('One');
       expect(snakeCase('___one___')).to.equal('one');
+    });
+
+    test.it('handles dot-separated', () => {
+      expect(kebabCase('OneOne.TwoTwo.ThreeThree')).to.equal('one-one.two-two.three-three');
+      expect(camelCase('OneOne.TwoTwo.ThreeThree')).to.equal('oneOne.twoTwo.threeThree');
+      expect(pascalCase('OneOne.TwoTwo.ThreeThree')).to.equal('OneOne.TwoTwo.ThreeThree');
+      expect(snakeCase('OneOne.TwoTwo.ThreeThree')).to.equal('one_one.two_two.three_three');
     });
 
     test.it('handles words with spaces', () => {
@@ -35,16 +46,22 @@ describe('services', () => {
       expect(camelCase('one two')).to.equal('oneTwo');
       expect(camelCase('one two three')).to.equal('oneTwoThree');
 
+      expect(pascalCase('one two')).to.equal('OneTwo');
+      expect(pascalCase('one two three')).to.equal('OneTwoThree');
+
       expect(snakeCase('one two')).to.equal('one_two');
       expect(snakeCase('one two three')).to.equal('one_two_three');
     });
 
-    test.it('handles two words with extra space', () => {
+    test.it('handles multiple words with extra space', () => {
       expect(kebabCase('one  two')).to.equal('one-two');
       expect(kebabCase('one  two  three')).to.equal('one-two-three');
 
       expect(camelCase('one  two')).to.equal('oneTwo');
       expect(camelCase('one  two  three')).to.equal('oneTwoThree');
+
+      expect(pascalCase('one  two')).to.equal('OneTwo');
+      expect(pascalCase('one  two  three')).to.equal('OneTwoThree');
 
       expect(snakeCase('one  two')).to.equal('one_two');
       expect(snakeCase('one  two  three')).to.equal('one_two_three');
@@ -57,6 +74,9 @@ describe('services', () => {
       expect(camelCase('one_two')).to.equal('oneTwo');
       expect(camelCase('one_two_three')).to.equal('oneTwoThree');
 
+      expect(pascalCase('one_two')).to.equal('OneTwo');
+      expect(pascalCase('one_two_three')).to.equal('OneTwoThree');
+
       expect(snakeCase('one_two')).to.equal('one_two');
       expect(snakeCase('one_two_three')).to.equal('one_two_three');
     });
@@ -67,6 +87,9 @@ describe('services', () => {
 
       expect(camelCase('oneTwo')).to.equal('oneTwo');
       expect(camelCase('oneTwoThree')).to.equal('oneTwoThree');
+
+      expect(pascalCase('oneTwo')).to.equal('OneTwo');
+      expect(pascalCase('oneTwoThree')).to.equal('OneTwoThree');
 
       expect(snakeCase('oneTwo')).to.equal('one_two');
       expect(snakeCase('oneTwoThree')).to.equal('one_two_three');
@@ -79,6 +102,9 @@ describe('services', () => {
       expect(camelCase('OneTwo')).to.equal('oneTwo');
       expect(camelCase('OneTwoThree')).to.equal('oneTwoThree');
 
+      expect(pascalCase('OneTwo')).to.equal('OneTwo');
+      expect(pascalCase('OneTwoThree')).to.equal('OneTwoThree');
+
       expect(snakeCase('OneTwo')).to.equal('one_two');
       expect(snakeCase('OneTwoThree')).to.equal('one_two_three');
     });
@@ -90,6 +116,9 @@ describe('services', () => {
       expect(camelCase('One1Two')).to.equal('one1Two');
       expect(camelCase('One1Two2Three')).to.equal('one1Two2Three');
 
+      expect(pascalCase('One1Two')).to.equal('One1Two');
+      expect(pascalCase('One1Two2Three')).to.equal('One1Two2Three');
+
       expect(snakeCase('One1Two')).to.equal('one1_two');
       expect(snakeCase('One1Two2Three')).to.equal('one1_two2_three');
     });
@@ -100,6 +129,9 @@ describe('services', () => {
 
       expect(camelCase('one-two')).to.equal('oneTwo');
       expect(camelCase('one-two-three')).to.equal('oneTwoThree');
+
+      expect(pascalCase('one-two')).to.equal('OneTwo');
+      expect(pascalCase('one-two-three')).to.equal('OneTwoThree');
 
       expect(snakeCase('one-two')).to.equal('one_two');
       expect(snakeCase('one-two-three')).to.equal('one_two_three');
