fd4ffd85326808f7170cabd9583f2749ff06fe92
Sam Harrison <sharrison@twilio.com>
2020-02-27 13:41:56 -0800

    fix: update the wording for the env vars help message

--- src/services/messaging/help-messages.js
+++ src/services/messaging/help-messages.js
@@ -1,15 +1,17 @@
 const { CLI_NAME } = require('../config');
 
-exports.HELP_ENVIRONMENT_VARIABLES = `You can also use credentials stored in environment variables:
+exports.HELP_ENVIRONMENT_VARIABLES = `Alternatively, ${CLI_NAME} can use credentials stored in environment variables:
 
-#  OPTION 1 (recommended)
+# OPTION 1 (recommended)
 TWILIO_ACCOUNT_SID = your Account SID from twil.io/console
 TWILIO_API_KEY = an API Key created at twil.io/get-api-key
 TWILIO_API_SECRET = the secret for the API Key
 
 # OPTION 2
 TWILIO_ACCOUNT_SID = your Account SID from twil.io/console
-TWILIO_AUTH_TOKEN = your Auth Token from twil.io/console`;
+TWILIO_AUTH_TOKEN = your Auth Token from twil.io/console
+
+Once these environment variables are set, a ${CLI_NAME} profile is not required to move forward with installation.`;
 
 exports.UNEXPECTED_ERROR = `${CLI_NAME} encountered an unexpected error. \
 To report this issue, execute the command with the "-l debug" flag, then copy the output to a new issue here: \
