d4d8b82f0f36810ab98de2e238a5e00f58a8550f
Sam Harrison <sharrison@twilio.com>
2020-06-23 09:27:24 -0500

    feat: update the env var message to use the proper OS syntax
    Windows uses the 'set' command while *nix requires the 'export' command.

--- src/services/messaging/help-messages.js
+++ src/services/messaging/help-messages.js
@@ -1,15 +1,17 @@
 const { CLI_NAME } = require('../config');
 
+const ENV_VAR_CMD = process.platform === 'win32' ? 'set' : 'export';
+
 exports.HELP_ENVIRONMENT_VARIABLES = `Alternatively, ${CLI_NAME} can use credentials stored in environment variables:
 
 # OPTION 1 (recommended)
-TWILIO_ACCOUNT_SID = your Account SID from twil.io/console
-TWILIO_API_KEY = an API Key created at twil.io/get-api-key
-TWILIO_API_SECRET = the secret for the API Key
+${ENV_VAR_CMD} TWILIO_ACCOUNT_SID=your Account SID from twil.io/console
+${ENV_VAR_CMD} TWILIO_API_KEY=an API Key created at twil.io/get-api-key
+${ENV_VAR_CMD} TWILIO_API_SECRET=the secret for the API Key
 
 # OPTION 2
-TWILIO_ACCOUNT_SID = your Account SID from twil.io/console
-TWILIO_AUTH_TOKEN = your Auth Token from twil.io/console
+${ENV_VAR_CMD} TWILIO_ACCOUNT_SID=your Account SID from twil.io/console
+${ENV_VAR_CMD} TWILIO_AUTH_TOKEN=your Auth Token from twil.io/console
 
 Once these environment variables are set, a ${CLI_NAME} profile is not required to move forward with installation.`;
 
