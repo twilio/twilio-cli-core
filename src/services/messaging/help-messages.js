477867bade536a9ddf69bba15a7bd241d87bc6fa
childish-sambino <sharrison@twilio.com>
2020-06-26 15:35:59 -0500

    Update help-messages.js

--- src/services/messaging/help-messages.js
+++ src/services/messaging/help-messages.js
@@ -14,7 +14,7 @@ exports.HELP_ENVIRONMENT_VARIABLES = `Alternatively, ${CLI_NAME} can use credent
 
 ${ENV_VARS_USAGE}
 
-Once these environment variables are set, a ${CLI_NAME} profile is not required to move forward with installation.`;
+Once these environment variables are set, a ${CLI_NAME} profile is not required and you may skip the "login" step.`;
 
 exports.ACCESS_DENIED = `${CLI_NAME} profiles use Standard API Keys which are not permitted to manage Accounts (e.g., create Subaccounts) and other API Keys. If you require this functionality a Master API Key or Auth Token must be stored in environment variables:
 
