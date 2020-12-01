34bebc177936751f62123a7e9feb31181fdb2577
shwetharadhakrishna <sradhakrishna@twilio.com>
2020-12-01 09:54:46 -0700

    Added test for preview

--- test/services/twilio-api/api-browser.test.js
+++ test/services/twilio-api/api-browser.test.js
@@ -19,6 +19,7 @@ describe('services', () => {
         expect(spec).to.have.property('studio_v1');
         expect(spec).to.have.property('studio_v2');
         expect(spec).to.have.property('api_v2010');
+        expect(spec).to.have.property('preview');
         expect(browser.domains.studio.paths['/v1/Flows'].operations.get).to.exist;
         expect(browser.domains.studio.paths['/v2/Flows'].operations.get).to.exist;
       });
