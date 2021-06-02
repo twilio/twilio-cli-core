2e3f0c796ee66cdb6bba29da0d72d1ab623795ce
Anuj <abadhwar@twilio.com>
2021-06-02 19:08:37 +0530

    Clear out comments

--- src/services/config.js
+++ src/services/config.js
@@ -146,7 +146,6 @@ class ConfigData {
     this.edge = configObj.edge;
     this.email = configObj.email || {};
     this.prompts = configObj.prompts || {};
-    // Note the historical 'projects' naming.
     configObj.projects = configObj.projects || [];
     configObj.projects.forEach((project) => this.addProfile(project.id, project.accountSid, project.region));
     this.setActiveProfile(configObj.activeProject);
@@ -180,7 +179,6 @@ class Config {
       edge: configData.edge,
       email: configData.email,
       prompts: configData.prompts,
-      // Note the historical 'projects' naming.
       projects: configData.projects,
       activeProject: configData.activeProfile,
     };
