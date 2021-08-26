01a4fc5be7fe0074b35e6b40997b54ed3301623d
lakshmiravali <lakshmiravali.rimmalapudi@gmail.com>
2021-08-26 16:44:26 +0530

    feat: Enable GitHub actions. Changes: 1. Added workflow file 2. Added scripts to update the changelog in cli-core 3. Added the actions to bump the version and release to GitHub

---
+++ scripts/change-log-helper.js
@@ -0,0 +1,117 @@
+const fs = require('fs');
+const readline = require('readline');
+
+const { logger } = require('../src/services/messaging/logging');
+
+const defaultVersionRegex = /(\d+)\.(\d+)\.(\d+)/;
+const defaultDateRegex = /\d{4}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])/;
+const cliCoreChangelogFile = 'CHANGES.md';
+const oaiChangelogFile = 'OAI_CHANGES.md';
+
+class ChangeLogHelper {
+  constructor(
+    cliCoreChangelogFilename = cliCoreChangelogFile,
+    oaiChangelogFilename = oaiChangelogFile,
+    versionRegex = defaultVersionRegex,
+    dateRegex = defaultDateRegex,
+  ) {
+    this.versionRegex = versionRegex;
+    this.dateRegex = dateRegex;
+    this.cliCoreChangelogFilename = cliCoreChangelogFilename;
+    this.oaiChangelogFilename = oaiChangelogFilename;
+    this.logger = logger;
+  }
+
+  async getAllReleaseVersionsFromGivenDate(date) {
+    this.logger.info(`Started detecting the versions from the date: ${date}`);
+    const versions = [];
+    const readLine = await this.getReadLiner(this.oaiChangelogFilename);
+    for await (const line of readLine) {
+      const currentDate = this.dateRegex.exec(line);
+      if (currentDate) {
+        const version = this.versionRegex.exec(line);
+        if (version) {
+          versions.push(version[0]);
+        }
+        if (currentDate[0] <= date) {
+          break;
+        }
+      }
+    }
+    this.logger.info(`Detected Versions: ${versions}`);
+    return versions;
+  }
+
+  async getLatestChangelogGeneratedDate() {
+    this.logger.info('Started detecting the latest date in cli core changelog');
+    let latestDate;
+    const readLine = await this.getReadLiner(this.cliCoreChangelogFilename);
+    for await (const line of readLine) {
+      latestDate = this.dateRegex.exec(line);
+      if (latestDate) {
+        latestDate = latestDate[0];
+        this.logger.info(`Detected the latest Date: ${latestDate}`);
+        break;
+      }
+    }
+    return latestDate;
+  }
+
+  async getChangesAfterGivenDate(date) {
+    this.logger.info(`Started getting the changelog from given date: ${date}`);
+    let readLines = false;
+    let fileData = '';
+    const readLine = await this.getReadLiner(this.oaiChangelogFilename);
+    for await (const line of readLine) {
+      const currentDate = this.dateRegex.exec(line);
+      if (currentDate) {
+        if (currentDate[0] > date) {
+          this.logger.info('Reading the lines');
+          readLines = true;
+        } else {
+          this.logger.info(`Changes from OpenAPI specs: ${fileData}`);
+          break;
+        }
+      } else if (readLines) {
+        fileData += `${line}\n`;
+      }
+    }
+    return fileData;
+  }
+
+  async appendChangesToChangelog() {
+    this.logger.info('Started getAndAppendChangesToChangelog');
+    const latestDate = await this.getLatestChangelogGeneratedDate(); // changes.md
+    if (latestDate) {
+      const changeLog = await this.getChangesAfterGivenDate(latestDate); // oai_changes.md
+      if (changeLog) {
+        try {
+          this.logger.info('Updating the CHANGES.md');
+          const data = fs.readFileSync(this.cliCoreChangelogFilename);
+          const fd = fs.openSync(this.cliCoreChangelogFilename, 'w+');
+          const insert = Buffer.from(changeLog);
+          fs.writeSync(fd, insert, 0, insert.length, 0);
+          fs.writeSync(fd, data, 0, data.length, insert.length);
+          fs.close(fd, (err) => {
+            if (err) throw err;
+          });
+          fs.writeFileSync('changeLog.md', changeLog);
+        } catch (error) {
+          this.logger.error(`Error while updating the changelog: ${error}`);
+        }
+        return changeLog;
+      }
+    }
+    return '';
+  }
+
+  async getReadLiner(filename) {
+    const fileStream = fs.createReadStream(filename);
+    return readline.createInterface({
+      input: fileStream,
+    });
+  }
+}
+module.exports = {
+  ChangeLogHelper,
+};
---
+++ scripts/update-api-spec-with-changelog.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+echo "Copying api-definitions"
+cp -R ~/oai_definitions/json/. src/services/twilio-api/
+echo "Running update changelog script"
+node scripts/update-change-log.js
+changeLog=$(cat changeLog.md)
+rm -rf changeLog.md
+if [ "$changeLog" != '' ]; then
+  changeLog="${changeLog//'%'/'%25'}"
+  changeLog="${changeLog//$'\n'/'%0A'}"
+  changeLog="${changeLog//$'\r'/'%0D'}"
+fi
+echo "Changelog: $changeLog"
+versionType=$(node scripts/get-version-type.js | tail -n -1)
+echo "Version type: $versionType"
+rm -rf OAI_CHANGES.md
+echo "Git configurations"
+git config --global user.email "lakshmiravali.rimmalapudi@gmail.com"
+git config --global user.name "lakshmiravali"
+branch=$(git branch --show-current)
+echo "Current branch: $branch"
+git add -A
+if [ -n "$(git status --porcelain)" ]; then
+  echo "There are changes to commit.";
+  commitMessage=''
+  if [ "$versionType" == 0 ] || [ "$versionType" == 1 ]
+  then
+    commitMessage='feat: Updated api definitions'
+  elif [ "$versionType" == 2 ]
+  then
+    commitMessage='fix: Updated api definitions'
+  else
+    echo "Invalid versionType: $versionType";
+    exit
+  fi
+  echo "Commit message:$commitMessage"
+  git commit -m "$commitMessage"
+  git push origin "$branch"
+else
+  echo "No changes to commit";
+fi
