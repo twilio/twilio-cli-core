77490304800997647cc291c13fac24b6bc2b4c04
ravali-rimmalapudi <83863595+ravali-rimmalapudi@users.noreply.github.com>
2021-10-05 15:18:49 +0530

    fix:Added the following changes: (#161)
    * fix:Added the following changes:
    1. Updated OAI commit not to include in the release notes
    2. Updated github dependencies
    3. Eslint fixes
    
    * Removed the emoji.

--- .github/scripts/trigger-workflow.js
+++ .github/scripts/trigger-workflow.js
@@ -8,45 +8,39 @@ const { Octokit } = require('@octokit/rest');
 const triggerWorkflow = async () => {
   try {
     const octokit = new Octokit({
-      auth: process.env.REPO_WORKFLOW_TOKEN
+      auth: process.env.REPO_ACCESS_TOKEN,
     });
     const workflowRef = process.env.WORKFLOW_NAME;
     const ref = process.env.BRANCH_NAME;
-    const [owner, repo] = process.env.REPO_NAME
-      ? process.env.REPO_NAME.split('/')
-      : [null, null];
+    const [owner, repo] = process.env.REPO_NAME ? process.env.REPO_NAME.split('/') : [null, null];
 
     // Decode inputs, this MUST be a valid JSON string
-    let inputs = {}
+    let inputs = {};
     const inputsJson = process.env.INPUTS;
-    if(inputsJson) {
-      inputs = JSON.parse(inputsJson)
+    if (inputsJson) {
+      inputs = JSON.parse(inputsJson);
     }
 
     const workflow = await octokit.rest.actions.getWorkflow({
       owner,
       repo,
-      workflow_id: workflowRef
+      workflow_id: workflowRef,
     });
 
-    core.info(`Workflow id is: ${workflow.data.id}`)
+    core.info(`Workflow id is: ${workflow.data.id}`);
 
     const dispatchResp = await octokit.rest.actions.createWorkflowDispatch({
       owner,
       repo,
       workflow_id: workflow.data.id,
       ref,
-      inputs
+      inputs,
     });
-    core.info(`API response status: ${dispatchResp.status} 🚀`)
+    core.info(`API response status: ${dispatchResp.status}.`);
   } catch (error) {
-    core.setFailed(error.message)
+    core.setFailed(error.message);
   }
-}
-
-(async () => {
-  await triggerWorkflow();
-})();
+};
 
 module.exports = {
   triggerWorkflow,
