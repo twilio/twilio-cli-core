8128630084d98ce5844884cd1851489e109619cb
sraghav <sraghav@twilio.com>
2022-02-10 14:55:25 +0530

    Changes to validate CLI tokens

---
+++ .github/scripts/validate_cli_tokens.py
@@ -0,0 +1,9 @@
+import requests
+import time
+
+time.sleep(60) #delay added as we need the CLI token validation workflow to complete
+
+github_url="https://api.github.com/repos/twilio/twilio-cli/actions/workflows/release-token-validation.yml/runs"
+response = requests.get(github_url)
+output=response.json()
+print(output['workflow_runs'][0]['conclusion'])
