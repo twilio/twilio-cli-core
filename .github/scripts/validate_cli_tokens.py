497c50fde61a51e075a562a9e0ff8ceb634b060a
manisha1997 <singhmanisha.250025@gmail.com>
2025-05-07 16:29:27 +0530

    chore: add cli core tag

--- .github/scripts/validate_cli_tokens.py
+++ .github/scripts/validate_cli_tokens.py
@@ -6,4 +6,5 @@ time.sleep(60) #delay added as we need the CLI token validation workflow to comp
 github_url="https://api.github.com/repos/twilio/twilio-cli/actions/workflows/release-token-validation.yml/runs"
 response = requests.get(github_url)
 output=response.json()
+print("sddbdsfhfuvi", output)
 print(output['workflow_runs'][0]['conclusion'])
