1e3760d868133b93b41ba460630de0e95541733e
Manisha Singh <singhmanisha.250025@gmail.com>
2025-05-29 19:55:05 +0530

    chore: Cli integration test (#272)

--- test/release-scripts/get-version-type.test.js
+++ test/release-scripts/get-version-type.test.js
@@ -1,134 +1,69 @@
-const fs = require('fs');
-
+/* eslint-disable max-classes-per-file */
 const { expect, test } = require('@twilio/cli-test');
-const mock = require('mock-fs');
-
-const { getVersionType } = require('../../.github/scripts/get-version-type');
+const proxyquire = require('proxyquire');
 
 describe('release-scripts', () => {
   describe('get-version-type', () => {
-    afterEach(() => {
-      mock.restore();
-    });
-    test.it('Get version upgrade type - Error', async () => {
-      await expect(getVersionType()).to.be.rejectedWith('File not found: OAI_CHANGES.md');
-    });
     test.it('Get version upgrade type - Major', async () => {
-      mock({
-        'CHANGES.md':
-          '[2021-08-26] Version 2.28.1\n' +
-          '---------------------------\n' +
-          '**Test**\n' +
-          '- Changes for test\n' +
-          '\n',
-        'OAI_CHANGES.md':
-          '[2021-09-08] Version 2.0.0\n' +
-          '---------------------------\n' +
-          '**OAI**\n' +
-          '- Latest changes from OAI\n' +
-          '\n' +
-          '**Second change**\n' +
-          '- Added OAI change\n' +
-          '\n' +
-          '\n' +
-          '[2021-08-25] Version 1.20.1\n' +
-          '---------------------------\n' +
-          '**Previous OAI change**\n' +
-          '- Previous OAI\n' +
-          '- Test Change OAI\n' +
-          '\n' +
-          '\n',
+      mockChangeLogHelper = {
+        getLatestChangelogGeneratedDate: async () => '2021-09-08',
+        getAllReleaseVersionsFromGivenDate: async () => ['2.28.1', '1.21.0'],
+      };
+
+      const { getVersionType } = proxyquire('../../.github/scripts/get-version-type', {
+        './change-log-helper': {
+          ChangeLogHelper: class {
+            constructor() {
+              return mockChangeLogHelper;
+            }
+          },
+        },
       });
+
       const result = await getVersionType();
+
       expect(result).to.eq(0);
     });
     test.it('Get version upgrade type - Minor', async () => {
-      mock({
-        'CHANGES.md':
-          '[2021-08-26] Version 2.28.1\n' +
-          '---------------------------\n' +
-          '**Test**\n' +
-          '- Changes for test\n' +
-          '\n',
-        'OAI_CHANGES.md':
-          '[2021-09-08] Version 1.21.0\n' +
-          '---------------------------\n' +
-          '**OAI**\n' +
-          '- Latest changes from OAI\n' +
-          '\n' +
-          '**Second change**\n' +
-          '- Added OAI change\n' +
-          '\n' +
-          '\n' +
-          '[2021-08-25] Version 1.20.1\n' +
-          '---------------------------\n' +
-          '**Previous OAI change**\n' +
-          '- Previous OAI\n' +
-          '- Test Change OAI\n' +
-          '\n' +
-          '\n',
+      mockChangeLogHelper = {
+        getLatestChangelogGeneratedDate: async () => '2021-09-08',
+        getAllReleaseVersionsFromGivenDate: async () => ['2.28.0', '2.27.0'],
+      };
+
+      const { getVersionType } = proxyquire('../../.github/scripts/get-version-type', {
+        './change-log-helper': {
+          ChangeLogHelper: class {
+            constructor() {
+              return mockChangeLogHelper;
+            }
+          },
+        },
       });
+
       const result = await getVersionType();
+
       expect(result).to.eq(1);
     });
 
     test.it('Get version upgrade type - Patch', async () => {
-      mock({
-        'CHANGES.md':
-          '[2021-08-26] Version 2.28.1\n' +
-          '---------------------------\n' +
-          '**Test**\n' +
-          '- Changes for test\n' +
-          '\n',
-        'OAI_CHANGES.md':
-          '[2021-09-08] Version 1.20.2\n' +
-          '---------------------------\n' +
-          '**OAI**\n' +
-          '- Latest changes from OAI\n' +
-          '\n' +
-          '**Second change**\n' +
-          '- Added OAI change\n' +
-          '\n' +
-          '\n' +
-          '[2021-08-25] Version 1.20.1\n' +
-          '---------------------------\n' +
-          '**Previous OAI change**\n' +
-          '- Previous OAI\n' +
-          '- Test Change OAI\n' +
-          '\n' +
-          '\n',
+      mockChangeLogHelper = {
+        getLatestChangelogGeneratedDate: async () => '2021-09-08',
+        getAllReleaseVersionsFromGivenDate: async () => ['2.28.1', '2.28.0'],
+      };
+
+      const { getVersionType } = proxyquire('../../.github/scripts/get-version-type', {
+        './change-log-helper': {
+          ChangeLogHelper: class {
+            constructor() {
+              return mockChangeLogHelper;
+            }
+          },
+        },
       });
+
       const result = await getVersionType();
+
       expect(result).to.eq(2);
-    });
-    test.it('Get version upgrade type - Invalid', async () => {
-      mock({
-        'CHANGES.md':
-          '[2021-09-08] Version 2.28.1\n' +
-          '---------------------------\n' +
-          '**Test**\n' +
-          '- Changes for test\n' +
-          '\n',
-        'OAI_CHANGES.md':
-          '[2021-09-08] Version 1.20.2\n' +
-          '---------------------------\n' +
-          '**OAI**\n' +
-          '- Latest changes from OAI\n' +
-          '\n' +
-          '**Second change**\n' +
-          '- Added OAI change\n' +
-          '\n' +
-          '\n' +
-          '[2021-08-25] Version 1.20.1\n' +
-          '---------------------------\n' +
-          '**Previous OAI change**\n' +
-          '- Previous OAI\n' +
-          '- Test Change OAI\n' +
-          '\n' +
-          '\n',
-      });
-      const result = await getVersionType();
-      expect(result).to.eq(-1);
     });
   });
 });
