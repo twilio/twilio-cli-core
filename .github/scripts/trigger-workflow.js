19d901cf696d8465c13dc6271d894d8d11953fe9
manisha1997 <singhmanisha.250025@gmail.com>
2025-06-12 17:37:10 +0530

    fix: change require to import

--- .github/scripts/trigger-workflow.js
+++ .github/scripts/trigger-workflow.js
@@ -1,5 +1,5 @@
-import * as core from '@actions/core';
-import { Octokit } from '@octokit/rest';
+const core = require('@actions/core');
+const { Octokit } = await import('@octokit/rest');
 
 /**
  * Functionality from benc-uk/workflow-dispatch.
